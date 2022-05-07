496a5c6edc86414f6e563f15da4e3d97c268ed3f
Mohamed Osama <73117933+mohamedosamaeg@users.noreply.github.com>
2022-05-07 13:03:09 +0200

    Mo V2 Comments (hide problem in server db)

--- CpactyTaxi/Data/ApplicationDbContext.cs
+++ CpactyTaxi/Data/ApplicationDbContext.cs
@@ -7,9 +7,9 @@ namespace CpactyTaxi.Data
     public class ApplicationDbContext : IdentityDbContext
     {
         public const string ConnectionString = "Server=(localdb)\\mssqllocaldb;Database=aspnet-CpactyTaxi-E20ACFF1-7A20-4C6D-8C70-9C33A0465009;Trusted_Connection=True;MultipleActiveResultSets=true";
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> optionsBuilder)
-            : base(optionsBuilder)
-        { optionsBuilder.UseSqlServer(ConnectionString); }
+        //public ApplicationDbContext(DbContextOptions<ApplicationDbContext> optionsBuilder)
+           // : base(optionsBuilder)
+       // { optionsBuilder.UseSqlServer(ConnectionString); }
                     public DbSet<Adress> adress { get; set; }
 
     }
