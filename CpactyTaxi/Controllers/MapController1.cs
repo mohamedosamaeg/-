a1c7fd477ff49108cd420c0d59a08fe318e89db1
ElsherifAliSaad <AElsherif@urfu.me>
2022-05-28 16:31:19 +0500

    MapVr3

---
+++ CpactyTaxi/Controllers/MapController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CpactyTaxi.Controllers
+{
+    public class MapController1 : Controller
+    {
+        public IActionResult Map()
+        {
+            return View();
+        }
+    }
+}
