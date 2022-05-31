1c9322be481375e13fcb2059bef57139a4ef4673
Mohamed Osama <73117933+mohamedosamaeg@users.noreply.github.com>
2022-05-31 22:30:59 +0500

    Mo V1 Front

---
+++ CpactyTaxi/Controllers/LandingController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CpactyTaxi.Controllers
+{
+    public class LandingController : Controller
+    {
+        public IActionResult LandingIndex()
+        {
+            return View();
+        }
+        public IActionResult LoginIndex()
+        {
+            return View();
+        }
+        public IActionResult RegisterIndex()
+        {
+            return View();
+        }
+    }
+}
