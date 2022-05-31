b6c3c6f22a969fadfb98e8e176b57c1d761196b2
Mostafa-Thabet2020 <62522809+Mostafa-Thabet2020@users.noreply.github.com>
2022-05-31 19:25:55 +0200

    UserAccount

---
+++ CpactyTaxi/Controllers/UserAccountController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CpactyTaxi.Controllers
+{
+    public class UserAccountController : Controller
+    {
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
