df24525d9a20f447f984b8f374518b72e5417aa1
Mohamed Osama <73117933+mohamedosamaeg@users.noreply.github.com>
2022-06-14 21:02:55 +0500

    mo Front

--- CpactyTaxi/Models/ErrorViewModel.cs
+++ CpactyTaxi/Models/ErrorViewModel.cs
@@ -3,7 +3,7 @@ using CpactyTaxi.Bases;
 namespace CpactyTaxi.Models
 {
     public class ErrorViewModel :Base
-    {
+    { 
         public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
