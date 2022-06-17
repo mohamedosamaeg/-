29c25eaa799349b8b8704880d04e97bd04f4715d
AAbdalkhamed@urfu.me <AAbdalkhamed@urfu.me>
2022-06-17 11:47:15 -0700

    mansctrlth

---
+++ CpactyTaxi/Controllers/TaxiServicesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CpactyTaxi.Controllers
+{
+    public class TaxiServicesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
